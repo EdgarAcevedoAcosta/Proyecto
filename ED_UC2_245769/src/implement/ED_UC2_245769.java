901f79173f2541266d46da8e051f64cbe478b03a
EdgarAcevedoAcosta <164127490+EdgarAcevedoAcosta@users.noreply.github.com>
2025-03-09 23:09:41 -0700

    Update ED_UC2_245769.java

--- ED_UC2_245769/src/implement/ED_UC2_245769.java
+++ ED_UC2_245769/src/implement/ED_UC2_245769.java
@@ -15,6 +15,37 @@ public class ED_UC2_245769 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Nodo a = new Nodo(1);
+        Nodo b = new Nodo(2);
+        Nodo c = new Nodo(3);
+        
+        Nodo d = new Nodo(13);
+        Nodo e = new Nodo(21);
+        
+        ListaEnlazadaSimple lista = new ListaEnlazadaSimple();
+        
+        lista.isEmply();
+        
+        lista.addFirst(a);
+        lista.addFirst(b);
+        lista.addFirst(c);
+        
+        lista.addFirst(new Nodo(7));
+        lista.addLast(new Nodo(42));
+        
+        lista.toString();
+        
+
+        System.out.println("tamaño: " + lista.Size());
+        System.out.println("---");
+        
+        lista.removeFirst();
+        lista.removeLast();
+        
+        
+        System.out.println("tamaño: " + lista.Size());
+
+        lista.isEmply();
     }
     
 }
