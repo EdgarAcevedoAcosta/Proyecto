d70739e2039b4a8c1e33d064a2487885a9796ce1
EdgarAcevedoAcosta <164127490+EdgarAcevedoAcosta@users.noreply.github.com>
2025-03-09 22:45:33 -0700

    main and nodo

---
+++ ED_UC2_245769/src/implement/Nodo.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package implement;
+
+/**
+ *
+ * @author edgar Arturo Acevedo Acosta
+ */
+public class Nodo {
+    int dato;
+    Nodo siguiente;
+
+    public Nodo(int dato) {
+        this.dato = dato;
+        this.siguiente = null;
+    }
+
+    public int getDato() {
+        return dato;
+    }
+
+    public void setDato(int dato) {
+        this.dato = dato;
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    @Override
+    public String toString() {
+        return "Nodo{" + "dato=" + dato + ", siguiente=" + siguiente + '}';
+    }
+    
+    
+}
